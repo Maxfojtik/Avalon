7291f9ee186815a1c879dc357ae65f698791b64c
Matthew <matthew.d.zirbel@gmail.com>
2020-09-04 01:18:41 -0400

    start on webpage

---
+++ Webpage/index.js
@@ -0,0 +1,14 @@
+
+// Joins a game from the start page. Will need to find the room code
+function joinGame() {
+
+}
+// Creates a game from the start page. Will need to ask server for a room code then go to that room
+function createGame() {
+
+}
+// Checks if the room code is valid via string checking and polling server. Enables join button only when valid
+$('#inputDatabaseName').on('input',function(e){
+    alert('Changed!')
+});
+
